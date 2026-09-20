00847783f252449ff56700a37f1604fdc51e0027
Erez <erez@users.noreply.github.com>
2026-09-20 12:00:06 +0000

    Publish edge-to-edge PWA icon

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='amsterdam-hadar-v6';
+const CACHE='amsterdam-hadar-v7';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-512.png','./apple-touch-icon.png','./favicon-32.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
